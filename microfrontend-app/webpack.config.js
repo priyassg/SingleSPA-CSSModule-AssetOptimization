57e68a56270d5fda68501c222b5013d07487b3ad
Priya Gupta <priya.s.gupta@jpmorgan.com>
2024-05-27 14:07:46 -0700

    Modifying webpack so it creates separate CSS assets

--- microfrontend-app/webpack.config.js
+++ microfrontend-app/webpack.config.js
@@ -1,7 +1,8 @@
-const { merge } = require("webpack-merge");
+const { merge, mergeWithRules } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -12,11 +13,41 @@ module.exports = (webpackConfigEnv, argv) => {
     disableHtmlGeneration: true, // This is required to not show the default html page
   });
 
-  return merge(defaultConfig, {
+  const customConfig = {
+    module: {
+      rules: [
+        {
+          test: /\.module\.css$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: "[local]__[hash:base64:5]__test",
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: "./src/index.ejs",
       }),
+      new MiniCssExtractPlugin({
+        filename: "styles.[contenthash].css",
+      }),
     ],
-  });
+  };
+
+  return mergeWithRules({
+    module: {
+      rules: {
+        test: "match",
+        use: "replace",
+      },
+    },
+  })(defaultConfig, customConfig);
 };
